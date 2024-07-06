81cb1ecc17fbe4f7c72fb4aa4d4d35b4127eef9e
cauan <cauandacruzferreira1010@gmail.com>
2024-07-06 09:26:53 -0300

    first commit

---
+++ JS/scripts.js
@@ -0,0 +1,15 @@
+document.getElementById('start').addEventListener('click', function() {
+    console.log('Start button clicked');
+});
+
+document.getElementById('pause').addEventListener('click', function() {
+    console.log('Pause button clicked');
+});
+
+document.getElementById('stop').addEventListener('click', function() {
+    console.log('Stop button clicked');
+});
+
+document.getElementById('add-to-playlist').addEventListener('click', function() {
+    console.log('Adicionar à Playlist do Spotify');
+});
